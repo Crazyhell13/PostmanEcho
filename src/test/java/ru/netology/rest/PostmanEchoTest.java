357893173fc62f7a861d4d453e5fee18de228603
crazyhell13 <crazyhell13@gmail.com>
2022-01-18 20:58:31 +0300

    Initial

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,33 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldPostToPostmanEcho (){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+
+    @Test
+    void shouldPostToPostmanEchoUTF (){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Ежики классные") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Ежики классные"));
+    }
+}
