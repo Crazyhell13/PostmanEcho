585af123c2094232969f3723a8651e7ccb3a7eec
crazyhell13 <crazyhell13@gmail.com>
2022-01-18 21:14:14 +0300

    Fixed

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -15,13 +15,14 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("so data"));
+                .body("data", equalTo("some data"));
     }
 
     @Test
     void shouldPostToPostmanEchoUTF (){
         given()
                 .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
                 .body("Ежики классные") // отправляемые данные (заголовки и query можно выставлять аналогично)
                 .when()
                 .post("/post")
